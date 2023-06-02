30161f7ffe653d6be8dd34cdb036568e34bcfeea
imteammy <kartoonn868@gmail.com>
2023-06-02 22:03:42 +0700

    add try dor mgdb

--- routes/route.js
+++ routes/route.js
@@ -3,27 +3,20 @@ const mongoose = require('mongoose');
 const route = express.Router();
 
 
-
-
-
-
 module.exports = (app) => {
     app.get('/hero', async (req, res) => {
-
-        // Connect MongoDB at default port 27017.
-     await  mongoose.connect('mongodb+srv://peerawat:YDCOyOKDb1LEyUV8@nodedev.fgnykzv.mongodb.net/', {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-        }).then((success) => {
-        const dbok = console.log("MongoDB connecting");
-        res.send(dbok);
-        res.end();
-        }).catch((err) => {
-        const dbnok =  console.log("Cannot connect to MongoDB" + err.message);
-        res.send(dbnok);
-        res.end();
+      try {
+        // Connect to MongoDB
+        await mongoose.connect('mongodb+srv://peerawat:YDCOyOKDb1LEyUV8@nodedev.fgnykzv.mongodb.net/', {
+          useNewUrlParser: true,
+          useCreateIndex: true,
         });
+  
+        console.log('MongoDB connected');
+        res.send('MongoDB connected');
+      } catch (err) {
+        console.error('Cannot connect to MongoDB:', err.message);
+        res.status(500).send('Cannot connect to MongoDB');
+      }
     });
-
-
-}
+  };
