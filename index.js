e37b528d361d3c833ead826e3d99cd3a39d2f592
imteammy <kartoonn868@gmail.com>
2023-06-02 15:36:31 -0700

    first commit

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
-const PORT = process.env.PORT || 3000
-const jwt = require('jsonwebtoken');
+const express   = require('express');
+const mongoose  = require('mongoose');
+const bodyPaser = require('body-parser')
+const flash     = require('connect-flash');
+const app       = express();
+const PORT      = process.env.PORT || 3000
+const jwt       = require('jsonwebtoken');
 
 
 const route = require('./routes/route');
 route(app);
 
-
+app.use(bodyPaser.urlencoded({ extended : true }));
+app.use(bodyPaser.json())
 
 
 app.listen(PORT,() => {
--- routes/route.js
+++ routes/route.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const route = express.Router();
+
+// Connect MongoDB at default port 27017.
+mongoose.connect('mongodb://localhost:27017/', {
+    //useNewUrlParser: true,
+    //useCreateIndex: true,
+}).then(() => {
+    console.log("MongoDb connecting");
+}).catch(() => {
+    console.log("Cannot connect to MongoDB");
+})
 
 
 module.exports = (app) => {
     app.get('/', (req, res) => {
         res.send("Hello world");
     });
+
+    
+
 }
