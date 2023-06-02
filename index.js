70e3b7650db9d53118b5ae4ee1dbba6cf01ffdde
imteammy <kartoonn868@gmail.com>
2023-06-02 16:06:14 +0700

    fix

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 const express   = require('express');
-const mongoose  = require('mongoose');
-const bodyPaser = require('body-parser')
-const flash     = require('connect-flash');
-const app       = express();
+
 const PORT      = process.env.PORT || 3000
-const jwt       = require('jsonwebtoken');
 
 
 const route = require('./routes/route');
 route(app);
-
-app.use(bodyPaser.urlencoded({ extended : true }));
-app.use(bodyPaser.json())
 
 app.use(flash());
 app.use()
