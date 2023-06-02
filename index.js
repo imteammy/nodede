6710ed251666a9d6fc09a7946bd7b449f90142c9
imteammy <kartoonn868@gmail.com>
2023-06-02 16:00:36 +0700

    fix

--- index.js
+++ index.js
@@ -13,9 +13,24 @@ route(app);
 app.use(bodyPaser.urlencoded({ extended : true }));
 app.use(bodyPaser.json())
 
+app.use(flash());
+app.use()
+
 app.get('/', (req, res) => {
     res.send('Hello Word!');
-})
+});
+
+app.get('/about', (req, res) => {
+    res.send('About Word!');
+});
+
+app.get('/contact', (req, res) => {
+    res.send('Contact Word!');
+});
+
+app.get('/login', (req, res) => {
+    res.send('Login Word!');
+});
 
 // app.listen(PORT,() => {
 //     console.log(`Server is runnig on http://localhost:${PORT}`);
