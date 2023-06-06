e1bfde6b84e40166c23288b0edde2dbf1c99102d
imteammy <kartoonn868@gmail.com>
2023-06-06 15:58:35 +0700

    add uoload

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended : true }));
+
 const routes = require('./routes/route');
 routes(app);
 
--- routes/route.js
+++ routes/route.js
@@ -1,27 +1,120 @@
 const express = require('express');
-const { Hero, Items, Roons } = require("../models/rov_model");
-const { CheckItems, greet } = require('../controllers/items');
+const {
+    Hero,
+    Items,
+    Roons
+} = require("../models/rov_model");
+const {
+    CheckItems,
+    greet
+} = require('../controllers/items');
 const db = require('../controllers/database');
 const route = express.Router();
 
 
 module.exports = (app) => {
-  app.get("/", (req, res) => {
-    res.send("Home");
-  });
+    app.get("/", (req, res) => {
+        res.send("Home");
+    });
 
-  //? Heroes
-  app.get("/hero", async (req, res) => {
+    //? Heroes
+    app.get("/hero", async (req, res) => {
 
-    async function run() {
+        async function run() {
 
-      await db;
-    
-      const data =   await Hero.find();
-      return res.send(data);
-    }
-    
-    run();
-  });
+            await db;
+
+            const data = await Hero.find();
+            return res.send(data);
+        }
+
+        run();
+    });
+
+    //TODO: Update hero
+    app.post("/hero/upload", async (req, res) => {
+
+        const {
+            token,
+            heroName,
+            heroImage,
+            heroStory,
+            mainRole,
+            subRole,
+            passiveSkillName,
+            passiveSkillImage,
+            passiveSkilleffect,
+            passiveSkillCooldown,
+            firstSkillName,
+            firstSkillImage,
+            firstSkilleffect,
+            firstSkillCooldown,
+            secondSkillName,
+            secondSkillImage,
+            secondSkilleffect,
+            secondSkillCooldown,
+            ultimateSkillName,
+            ultimateSkillImage,
+            ultimateSkilleffect,
+            ultimateSkillCooldown
+        } = req.body;
+
+        if (token != "123") {
+            return res.status(400).send("No token provided");
+        }
+
+        const upload = {
+            name: heroName,
+            image: heroImage,
+            story: heroStory,
+            role: {
+                main: mainRole,
+                sub: subRole
+            },
+            skills: {
+                passiveSkill: {
+                    name: passiveSkillName,
+                    image: passiveSkillImage,
+                    effect: passiveSkilleffect,
+                    cooldown: passiveSkillCooldown
+                },
+                firstSkill: {
+                    name: firstSkillName,
+                    image: firstSkillImage,
+                    effect: firstSkilleffect,
+                    cooldown: firstSkillCooldown
+                },
+                secondSkill: {
+                    name: secondSkillName,
+                    image: secondSkillImage,
+                    effect: secondSkilleffect,
+                    cooldown: secondSkillCooldown
+                },
+                ultimateSkill: {
+                    name: ultimateSkillName,
+                    image: ultimateSkillImage,
+                    effect: ultimateSkilleffect,
+                    cooldown: ultimateSkillCooldown
+                }
+            }
+
+        }
+
+        async function update() {
+
+            await db;
+            const filter = req.body;
+            const data = await Hero.create(upload).then(success => {
+                return res.send(success);
+            }).catch(error => {
+                return res.send(error);
+            });
+
+
+        }
+
+        update();
+
+    });
 
 };
