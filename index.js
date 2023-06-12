6552e4caf2ea25ac6ec55fbe748e51bc939232e9
imteammy <kartoonn868@gmail.com>
2023-06-13 00:55:14 +0700

    Test ok

--- index.js
+++ index.js
@@ -8,12 +8,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //? Middleware, Route, Database
-const auth = require("./middleware/middleware");
+//const auth = require("./middleware/middleware");
 const routes = require("./routes/route");
 const connectDB = require("./configs/database");
 routes(app);
 connectDB();
+function auth(req, res, next) {
+  const { token } = req.body;
 
+  if (!token || token === "" || token === undefined) {
+    return res.json({error : "Token is required"});
+  }
+
+  if (token === "12345") {
+    next();
+  }
+}
 app.use(auth);
 app.use((req, res, next) => {
   return res.status(404).json({ error: "Page not found!" });
--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,14 +1,4 @@
-function auth(req, res, next) {
-    const { token } = req.body;
-  
-    if (!token || token === "" || token === undefined) {
-      return res.json({error : "Token is required"});
-    }
-  
-    if (token === "12345") {
-      next();
-    }
-  }
+
   
   module.exports = auth;
   
