81df527fd5e5ae4a23163c15214295fedd26668e
imteammy <kartoonn868@gmail.com>
2023-06-13 10:15:45 +0700

    fix get

---
+++ controllers/herocombos.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const mongoose = require("mongoose");
+//
+const { HeroCombo } = require("../models/rov_models");
+
+exports.getAllHeroes = async (req, res, next) => {
+  try {
+    const hero = await Hero.find();
+
+    if (hero.length === 0 ){
+      return res.json({message : "Heroes is empty."})
+    }
+
+    return res.send(hero);
+  } catch (error) {
+    return res.send(error.message);
+  }
+};
+
+exports.getHeroByName = async (req, res, next) => {
+  const name = req.body.name;
+  try {
+    const hero = await Hero.find({ name });
+    return res.send(hero);
+  } catch (error) {
+    return res.send(error.message);
+  }
+};
+
+exports.addHero = async (req, res, next) => {
+  const HeroData = req.body;
+
+  try {
+    const result = await Hero.create(HeroData);
+    return res.json({ message: "Created new hero successfully.", data: result });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+exports.updateHeroByName = async (req, res, next) => {
+  const { name, updatedData } = req.body;
+
+  try {
+    const updatedHero = await Hero.findOneAndUpdate(
+      { name: name },
+      updatedData,
+      { new: true }
+    );
+
+    if (!updatedHero) {
+      return res.status(404).json({ message: "Hero not found." });
+    }
+
+    return res.json({ message: "Hero updated successfully.", data: updatedHero });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+exports.deleteHeroByName = async (req, res, next) => {
+  const { name } = req.body;
+  try {
+    const deleteHero = await Hero.findOneAndDelete({ name });
+    if (!deleteHero) {
+      return res.status(404).json({ message: "Hero not found." });
+    }
+    
+    return res.json({ message: "Hero deleted successfully.", data: deleteHero });
+
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+exports.deleteHeroById = async (req, res, next) => {
+  const { id } = req.body;
+  try {
+    const deleteHero = await Hero.findOneAndDelete({ _id : id });
+    if (!deleteHero) {
+      return res.status(404).json({ message: "Hero not found." });
+    }
+    
+    return res.json({ message: "Hero deleted successfully.", data: deleteHero });
+
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
--- routes/route.js
+++ routes/route.js
@@ -29,7 +29,7 @@ module.exports = (app) => {
 
   //? Heroes
   app.get('/hero/', heroController.getAllHeroes);
-  app.get('/hero/name', heroController.getHeroByName);
+  app.post('/hero/name', heroController.getHeroByName);
   app.post('/hero/add', auth, heroController.addHero);
   app.post('/hero/update', auth, heroController.updateHeroByName);
   app.delete('/hero/delete', auth, heroController.deleteHeroByName);
@@ -37,28 +37,28 @@ module.exports = (app) => {
 
   //? Items
   app.get('/items', itemsController.getAllItems);
-  app.get('/items/name', itemsController.getItemByName);
+  app.post('/items/name', itemsController.getItemByName);
   app.post('/items/add', auth, itemsController.addItem);
   app.post('/items/update', auth, itemsController.updateItem);
   app.delete('/items/delete', auth, itemsController.deleteItem);
 
   //? Challengers Skills
   app.get('/challengersskill', challengersSkillController.getAllSkills);
-  app.get('/challengersskill/name', challengersSkillController.getSkillByName);
+  app.post('/challengersskill/name', challengersSkillController.getSkillByName);
   app.post('/challengersskill/add', auth, challengersSkillController.addSkill);
   app.post('/challengersskill/update', auth, challengersSkillController.updateSkill);
   app.delete('/challengersskill/delete', auth, challengersSkillController.deleteSkill);
 
   //? Roons
   app.get('/roon', roonController.getAllRoons);
-  app.get('/roon/name', roonController.getRoonByName);
+  app.post('/roon/name', roonController.getRoonByName);
   app.post('/roon/add', auth, roonController.addRoon);
   app.post('/roon/update', auth, roonController.updateRoonByName);
   app.delete('/roon/delete', auth, roonController.deleteRoon);
 
   //? LatensSkills
   app.get('/latensskill', latensSkillController.getAllLatensSkills);
-  app.get('/latensskill/name', auth, latensSkillController.getLatensSkillByName);
+  app.post('/latensskill/name', auth, latensSkillController.getLatensSkillByName);
   app.post('/latensskill/add', auth, latensSkillController.addLatensSkill);
   app.post('/latensskill/update', auth, latensSkillController.updateLatensSkill);
   app.delete('/latensskill/delete', auth, latensSkillController.deleteLatensSkill);
