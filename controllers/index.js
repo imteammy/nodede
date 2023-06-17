05df028bcc9cda3c5e5c45f4aeb1b35a0bc5dc73
imteammy <kartoonn868@gmail.com>
2023-06-17 16:05:16 +0700

    update route

---
+++ controllers/index.js
@@ -0,0 +1,13 @@
+const hero = require('../controllers/heroes');
+const items = require('../controllers/items');
+const roon = require('../controllers/roons');
+const latensSkill = require('../controllers/latensSkills');
+const challengersSkill = require('../controllers/challengerSkills');
+
+module.exports = {
+    hero,
+    items,
+    roon,
+    latensSkill,
+    challengersSkill
+};
