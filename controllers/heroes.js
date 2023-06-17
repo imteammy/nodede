ca235ffdf0edac1bacfa5720c4a98203e8242901
imteammy <kartoonn868@gmail.com>
2023-06-18 00:11:38 +0700

    fix heru update3

--- controllers/heroes.js
+++ controllers/heroes.js
@@ -1,92 +1,95 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express')
+const mongoose = require('mongoose')
 //
-const { Hero } = require("../models/rov_models");
+const { Hero } = require('../models/rov_models')
 
 exports.getAllHeroes = async (req, res, next) => {
     try {
-        const hero = await Hero.find();
+        const hero = await Hero.find()
 
         if (hero.length === 0) {
-            return res.json({ message: "Heroes is empty." })
+            return res.json({ message: 'Heroes is empty.' })
         }
 
-        return res.send(hero);
+        return res.send(hero)
     } catch (error) {
-        return res.send(error.message);
+        return res.send(error.message)
     }
-};
+}
 
 exports.getHeroByName = async (req, res, next) => {
-    const name = req.body.name;
+    const name = req.body.name
     try {
-        const hero = await Hero.find({ name });
-        return res.send(hero);
+        const hero = await Hero.find({ name })
+        return res.send(hero)
     } catch (error) {
-        return res.send(error.message);
+        return res.send(error.message)
     }
-};
+}
 
 exports.addHero = async (req, res, next) => {
-    const HeroData = req.body;
+    const HeroData = req.body
 
     try {
-        const result = await Hero.create(HeroData);
-        return res.json({ message: "Created new hero successfully.", data: result });
+        const result = await Hero.create(HeroData)
+        return res.json({ message: 'Created new hero successfully.', data: result })
     } catch (error) {
-        return res.status(400).json({ message: error.message });
+        return res.status(400).json({ message: error.message })
     }
-};
+}
 
 exports.updateHeroByName = async (req, res, next) => {
     try {
-        const { findName } = req.body;
-        const data = JSON.parse(req.body);
+        const newData = JSON.parse(req.body);
+        delete newData.token;
+
+        const data = JSON.stringify(newData);
+        const { findName, ...update } = req.body; // Remove findName property from req.body
+
         const filter = { name: findName };
-        const update = Object.assign({}, data);
-        const UpdateResult = await Hero.findOneAndUpdate(
-            filter,
-            update,
-            { new: true }
-        );
+
+        const updateResult = await Hero.findOneAndUpdate(filter, update, {
+        new: true
+        });
+
 
         if (!UpdateResult) {
-            return res.status(404).json({ message: "Hero not found." });
-        }
+            return res.status(404).json({ message: 'Hero not found.' });
+        };
 
-        return res.json({ message: "Hero updated successfully.", data: UpdateResult });
+        return res.json({
+            message: 'Hero updated successfully.',
+            data: UpdateResult
+        });
     } catch (error) {
-        return res.status(400).json({ message: error.message });
+        return res.status(400).json({ message: error.message })
     }
-};
-
+}
 
 exports.deleteHeroByName = async (req, res, next) => {
-    const { name } = req.body;
+    const { name } = req.body
     try {
-        const deleteHero = await Hero.findOneAndDelete({ name });
+        const deleteHero = await Hero.findOneAndDelete({ name })
         if (!deleteHero) {
-            return res.status(404).json({ message: "Hero not found." });
+            return res.status(404).json({ message: 'Hero not found.' })
         }
 
-        return res.json({ message: "Hero deleted successfully.", data: deleteHero });
-
+        return res.json({ message: 'Hero deleted successfully.', data: deleteHero })
     } catch (error) {
-        return res.status(400).json({ message: error.message });
+        return res.status(400).json({ message: error.message })
     }
-};
+}
 
 exports.deleteHeroById = async (req, res, next) => {
-    const { id } = req.body;
+    const { id } = req.body
     try {
-        const deleteHero = await Hero.findOneAndDelete({ _id: id });
+        const deleteHero = await Hero.findOneAndDelete({ _id: id })
         if (!deleteHero) {
-            return res.status(404).json({ message: "Hero not found." });
+            return res.status(404).json({ message: 'Hero not found.' })
         }
 
-        return res.json({ message: "Hero deleted successfully.", data: deleteHero });
-
+        return res.json({ message: 'Hero deleted successfully.', data: deleteHero })
     } catch (error) {
-        return res.status(400).json({ message: error.message });
+        return res.status(400).json({ message: error.message })
     }
-};
+}
